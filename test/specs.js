d767860c2ff36b16e5f8b843be24ea8357ea02be
Mushex Antaranian <mushex@antaranian.com>
2015-01-29 17:17:05 +0400

    Implement HTML `lang` checking for elements

--- test/specs.js
+++ test/specs.js
@@ -14,38 +14,55 @@ describe('Itself', function () {
     });
 });
 
+
+describe('HTML `lang`', function(){
+    it('should get root\'s `lang` value', function(){
+        // no params
+        assert.equal(getLocale(), 'de');
+
+        // invalid selector
+        assert.equal(getLocale('#el-h2'), 'de');
+
+        // given element lacks of `lang`
+        assert.equal(getLocale('#el-h1'), 'de');
+    });
+
+    it('should get element\'s `lang` value', function(){
+        assert.equal(getLocale('#el-div'), 'hy');
+    });
+
+    it('should get element\'s closest parent\'s `lang` value', function(){
+        assert.equal(getLocale('#el-p'), 'hy');
+    });
+
+    after(function(){
+        // remove root's `lang` to evaluate later
+        document.documentElement.removeAttribute('lang');
+    });
+});
+
 describe('Cookies', function () {
+    it('should return cookie value if set', function () {
+        cookie('lang', 'hy');
+        assert.equal(getLocale(), 'hy');
+    });
+
+    it('should support cookie name as argument', function () {
+        cookie('locale', 'hy');
+
+        getLocale.configure({ cookie: 'locale' });
+        assert.equal(getLocale(), 'hy');
+    });
+
     after(function(){
         // cleanup
         cookie('lang', null);
         cookie('locale', null);
     });
-
-    it('should return cookie value if set', function () {
-        assert.equal(getLocale(), 'en-US');
-
-        cookie('lang', 'hy');
-
-        assert.equal(getLocale(), 'hy');
-    });
-
-    it('should support cookie name as argument', function () {
-        getLocale.configure({ cookie: 'locale' });
-
-        assert.equal(getLocale(), 'en-US');
-
-        cookie('locale', 'hy');
-
-        assert.equal(getLocale(), 'hy');
-    });
 });
 
-describe('HTML lang', function(){
-    it('should return HTML5 `lang` attribute if set', function(){
+describe('Navigator', function(){
+    it('should return navigator\'s preference', function(){
         assert.equal(getLocale(), 'en-US');
-
-        document.documentElement.setAttribute('lang', 'de');
-
-        assert.equal(getLocale(), 'de');
     });
 });
