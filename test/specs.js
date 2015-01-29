3ac03360fe41bb0801a9246208f40cdf2989adf2
Mushex Antaranian <mushex@antaranian.com>
2015-01-29 14:17:30 +0400

    Add support for cookies

--- test/specs.js
+++ test/specs.js
@@ -1,11 +1,25 @@
 var assert = require('assert');
 
+var cookie = require('cookie');
+
 var getLocale = require('../');
 
+
+var lang;
 
 describe('Itself', function () {
     it('should be proper type', function () {
         assert(getLocale);
         assert.equal(getLocale.length, 1);
+    });
+});
+
+describe('Cookies', function () {
+    it('should return cookie value if set', function () {
+        assert.equal(getLocale(), 'en-US');
+
+        cookie('lang', 'hy');
+
+        assert.equal(getLocale(), 'hy');
     });
 });
